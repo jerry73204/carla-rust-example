87cb2cf0e854dd3b467f3e12d55782f45079f369
aeon <jerry73204@gmail.com>
2023-09-12 17:33:16 +0800

    Make Ackermann control work.

--- automatic-control/src/main.rs
+++ automatic-control/src/main.rs
@@ -4,9 +4,10 @@ use carla::{
     rpc::{EpisodeSettings, VehicleAckermannControl},
 };
 use clap::Parser;
-use nalgebra::{UnitQuaternion, Vector3, Translation3, Isometry3};
-use rand::prelude::*;
+use nalgebra::{Isometry3, Translation3, UnitQuaternion};
+use noisy_float::prelude::*;
 use std::{
+    f32::consts::PI,
     sync::{
         atomic::{AtomicBool, Ordering},
         Arc,
@@ -17,16 +18,18 @@ use std::{
 fn main() -> Result<()> {
     let opts = Opts::parse();
 
-    // Create a random generator
-    let mut rng = rand::thread_rng();
-
     // Connect to the client and retrieve the world object
     let client = Client::connect(&opts.addr, opts.port, None);
 
     // Set the world
     let mut world = match &opts.world {
         Some(world) => client.load_world(world),
-        None => client.world(),
+        None => {
+            // It causes the client to crash.
+            // client.reload_world()
+
+            client.world()
+        }
     };
 
     // Set a/synchronous mode
@@ -43,13 +46,13 @@ fn main() -> Result<()> {
     let map = world.map();
 
     // spawn the vehicle in ...
-    let translation = Translation3::new(83.075226, 13.414804, 0.600000);
-    let rotation = UnitQuaternion::from_euler_angles(0.0000000, 0.000000, -179.840790f32.to_radians());
-
-    let start_point = Isometry3{translation, rotation};
-        
+    let start_point = Isometry3 {
+        translation: Translation3::new(83.075226, 13.414804, 0.600000),
+        rotation: UnitQuaternion::from_euler_angles(0.0, 0.0, -179.840_79_f32.to_radians()),
+    };
     eprintln!("Spawn a vehicle at {start_point}");
 
+    // Register a Ctrl-C handler
     let stop = Arc::new(AtomicBool::new(false));
     {
         let stop = stop.clone();
@@ -75,48 +78,64 @@ fn main() -> Result<()> {
         let vehicle_transform = vehicle.transform();
         let vehicle_location = vehicle_transform.translation;
 
-        let x = vehicle_location.x + 10.0;
-        let y = vehicle_location.y;
-        let z = vehicle_location.z + 7.0;
-        let translation = Translation3::new(x, y, z);
-        let rotation = UnitQuaternion::from_euler_angles(0.000000, -20.000000f32.to_radians(), -179.840790f32.to_radians());
-
-        let s_point = Isometry3{translation, rotation};
-        spectator.set_transform(&s_point);
-
-        // eprintln!("vehicle drives at {vehicle_location}");
-
         let Some(curr_waypoint) = map.waypoint(&vehicle_location) else {
             vehicle.set_transform(&start_point);
             continue;
         };
 
-        // Choose a next waypoint randomly
-        let next_waypoints: Vec<_> = curr_waypoint.next(1.0).iter().collect();
-        let Some(next_waypoint) = next_waypoints.choose(&mut rng) else {
+        // Choose a next waypoint
+        let Some(next_waypoint) = curr_waypoint.next(1.0).get(0) else {
             vehicle.set_transform(&start_point);
             continue;
         };
 
-        // Compute the offset vector from the car to the next waypoint
+        // Set the spectator viewpoint
+        let s_point = vehicle_transform * Translation3::new(-10.0, 0.0, 7.0);
+        spectator.set_transform(&s_point);
+
+        // Compute the displacement vector from the car to the next
+        // waypoint.
         let next_location = next_waypoint.transofrm().translation;
         let dir = next_location.vector - vehicle_transform.translation.vector;
 
-        // Create a rotation that looks at the direction of `dir`.
-        let up = Vector3::z();
-        let target_rot = UnitQuaternion::look_at_rh(&dir, &up);
+        // Compute the heading offset towards the next waypoint.
+        let heading_offset = {
+            let (_, _, current_yaw) = vehicle_transform.rotation.euler_angles();
+            let target_yaw = (dir.y).atan2(dir.x);
 
-        //TODO: fix degree of heading_offset
-        // Compute the angle offset from the vehicle heading to the target direction.
-        let heading_offset = vehicle_transform.rotation.angle_to(&target_rot).to_degrees();
+            let current_yaw = current_yaw; // Change range [0, 2π] to [-π, π]
+            let offset = target_yaw - current_yaw;
+
+            if offset >= PI {
+                offset - PI * 2.0
+            } else if offset <= -PI {
+                offset + PI * 2.0
+            } else {
+                offset
+            }
+        }
+        .to_degrees();
+
+        // Compute the steering ratio
+        let steer = {
+            let physics_control = vehicle.physics_control();
+            let max_steer_angle = physics_control
+                .wheels
+                .iter()
+                .map(|wheel| r32(wheel.max_steer_angle))
+                .max()
+                .expect("Unable to obtain max steering angle from the vehicle")
+                .raw();
+            (heading_offset / max_steer_angle).clamp(-1.0, 1.0)
+        };
 
         // Compute the steering speed
         let steer_speed = if heading_offset.abs() < 3.0 {
             0.0
         } else if heading_offset > 0.0 {
-            1.0
+            0.1
         } else {
-            -1.0
+            -0.1
         };
 
         // Get the current car speed.
@@ -125,12 +144,11 @@ fn main() -> Result<()> {
         // Compute the acceleration
         let acceleration = if vehicle_speed < 5.0 { 1.0 } else { 0.0 };
 
-
         // Apply the control to the car
         let control = VehicleAckermannControl {
             //TODO: the parameter of 'steer' has bug
-            steer: 0.0,
-            steer_speed: 0.0,
+            steer,
+            steer_speed,
             speed: opts.target_speed * 10.0 / 36.0,
             acceleration,
             jerk: 0.0,
@@ -164,6 +182,6 @@ struct Opts {
     #[clap(long)]
     pub world: Option<String>,
 
-    #[clap(long, default_value = "20.0")]
+    #[clap(long, default_value = "5.0")]
     pub target_speed: f32,
 }
